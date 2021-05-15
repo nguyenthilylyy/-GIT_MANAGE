a062d27c425c713c26fbe2c6f692df1cf79f9ffa
unknown <nguyenthilyly94@gmail.com>
2021-05-15 20:06:05 +0700

    01_Java: Add test2

---
+++ 01_Java/test2_AverageScore/index.js
@@ -0,0 +1,11 @@
+var toan;
+var van;
+var anh;
+
+toan = +prompt('nhap diem toan:');
+van = +prompt('nhap diem van:');
+anh = +prompt('nhap diem anh:');
+
+tong = toan + van + anh;
+tb = tong/3;
+alert('diem tb:' + tb);
