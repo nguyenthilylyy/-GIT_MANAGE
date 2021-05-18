bee129de1326597887285417e69eda3bc4347f40
unknown <nguyenthilyly94@gmail.com>
2021-05-18 19:51:25 +0700

    [01_Java] Bai tap ngay 18/5 + Bai1: Tinh tong 2 so + Bai2: Tinh lai suat ngan hang

---
+++ 01_Java/Baitap_Java/Bai1/my.js
@@ -0,0 +1,9 @@
+//Tinh tong 2 so
+
+let a = +prompt("nhap so thu 1:");
+let b = +prompt("nhap so thu 2:");
+sum=a+b;
+
+document.write("So thu nhat la: " + a );
+document.write("So thu hai la: " + b);
+document.write("Tong 2 so tren la: " + sum);
---
+++ 01_Java/Baitap_Java/Bai2/my.js
@@ -0,0 +1,8 @@
+//Tinh lai suat ngan hang
+
+let a = +prompt("Nhap so tien:");
+let b = +prompt("Nhap lai suat:");
+let c = +prompt("Nhap so nam:");
+laisuat = a + c * (a * b/100);
+
+document.write("So tien nhan lai sau " + c + " nam la:" + laisuat);
