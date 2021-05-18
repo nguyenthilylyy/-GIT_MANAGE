1d7145eccf033afffb71954821739e1068b2d940
unknown <nguyenthilyly94@gmail.com>
2021-05-18 20:11:47 +0700

    Bai3: Tinh chu vi va dien tich hinh tron

---
+++ 01_Java/Baitap_Java/Bai3/my.js
@@ -0,0 +1,8 @@
+//Tinh Chuvi va Dientich hinh tron
+
+let R = +prompt("Nhap ban kinh hinh tron: ");
+C = 2 * 3.14 * R;
+S = 3.14 * R * R
+
+document.write("Chu vi hinh tron la: " + C);
+document.write("<br> Dien tich hinh tron la: " + S);
